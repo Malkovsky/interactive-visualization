dda4f7e8d21bfc0ae0a4debb0b79fb8f328e6c0c
Malkovsky <malkovskynv@gmail.com>
2020-04-25 17:59:18 +0300

    Added notebook about prefix tree

--- animation_utils/animation.py
+++ animation_utils/animation.py
@@ -9,3 +9,12 @@ def animate_list(lst):
     return interact(step_slice,
                     lst=fixed(lst),
                     idx=widgets.IntSlider(min=0, max=len(lst)-1, step=1, value=0))
+
+def step_dict(dct, key):
+    return animate_list(dct[key]);
+
+def animate_dict(dct, description=None):
+    return interact(step_dict,
+                    dct=fixed(dct),
+                    key=widgets.Select(options=dct.keys(),
+                                       description=description));
