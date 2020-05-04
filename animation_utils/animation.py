0193019943fb5f4524605a1f587befe5ce4a9670
Malkovsky <malkovskynv@gmail.com>
2020-05-04 14:16:00 +0300

    Updated list animation

--- animation_utils/animation.py
+++ animation_utils/animation.py
@@ -1,14 +1,21 @@
 from __future__ import print_function
 from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
+from IPython.display import display
 
 def step_slice(lst, idx):
     return lst[idx]
 
-def animate_list(lst):
+def animate_list(lst, play=False):
+    slider = widgets.IntSlider(min=0, max=len(lst)-1, step=1, value=0)
+    if play:
+        play_widjet = widgets.Play()
+        widgets.jslink((play_widjet, 'value'), (slider, 'value'))
+        display(play_widjet)
+        #slider = widgets.Box([play_widject, slider])
     return interact(step_slice,
                     lst=fixed(lst),
-                    idx=widgets.IntSlider(min=0, max=len(lst)-1, step=1, value=0))
+                    idx=slider)
 
 def step_dict(dct, key):
     return animate_list(dct[key]);
