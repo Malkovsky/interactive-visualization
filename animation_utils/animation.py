e3e4da3daf94f65530bb75bc08d44873462c7e7e
malkovskiy <malkovskynv@gmail.com>
2020-09-28 17:16:02 +0300

    Updated with prev and next buttons for animation control

--- animation_utils/animation.py
+++ animation_utils/animation.py
@@ -8,13 +8,38 @@ def step_slice(lst, step):
     return lst[step]
 
 
-def animate_list(lst, play=False, interval=200):
+def animate_list(lst, play=False, navigation=True, interval=200):
     slider = widgets.IntSlider(min=0, max=len(lst) - 1, step=1, value=0)
+    
+    if navigation:
+        prev_button = widgets.Button(
+                description='Prev',
+                disabled=False,
+            )
+        next_button = widgets.Button(
+                description='Next',
+                disabled=False,
+            )
+        def increment_value(arg):
+            slider.value += 1
+        def decrement_value(arg):
+            slider.value -= 1
+            
+        prev_button.on_click(decrement_value)
+        next_button.on_click(increment_value)
+    
     if play:
-        play_widjet = widgets.Play(interval=interval)
-        widgets.jslink((play_widjet, 'value'), (slider, 'value'))
-        display(play_widjet)
-        # slider = widgets.Box([play_widject, slider])
+        play_widget = widgets.Play(interval=interval)
+        widgets.jslink((play_widget, 'value'), (slider, 'value'))
+        
+    if play or navigation:
+        if play and navigation:
+            display(widgets.HBox([play_widget, prev_button, next_button]))
+        elif play:
+            display(play_widjet)
+        elif navigation:
+            display(HBox([prev_button, next_button]))
+        
     return interact(step_slice,
                     lst=fixed(lst),
                     step=slider)
