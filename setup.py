de8d2135f4a1186db24302b3f40d8e76cf83d44b
Malkovsky <malkovskynv@gmail.com>
2021-11-15 00:05:39 +0300

    feat: small addition to graph visualization

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name="interactive-visualization", # Replace with your own username
-    version="0.1.3",
+    version="0.1.4",
     author="Nikolay Malkovsky",
     author_email="malkovskynv@gmail.com",
     description="Package provides a simple widget-based framework for interactive visualization of algorithms",
